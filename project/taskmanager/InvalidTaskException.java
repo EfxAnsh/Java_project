ee56fa71e80f13463e5568c7b3207b81e995af16
Aditya12155 <adityaddn07@gmail.com>
2025-03-18 13:30:36 +0530

    Updated InvalidTaskException.java
    comment is added.

--- project/taskmanager/InvalidTaskException.java
+++ project/taskmanager/InvalidTaskException.java
@@ -3,5 +3,6 @@ package project.taskmanager;
 public class InvalidTaskException extends Exception {
     public InvalidTaskException(String message) {
         super(message);
+        //DONE
     }
 }
